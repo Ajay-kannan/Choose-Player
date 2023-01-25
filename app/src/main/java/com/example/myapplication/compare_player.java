3943c3eaa25216d1e31cc419ad966550b16820a6
Ajay <ajaykannan04102002@gmail.com>
2023-01-25 15:07:19 +0530

    initial_commit

--- app/src/main/java/com/example/myapplication/compare_player.java
+++ app/src/main/java/com/example/myapplication/compare_player.java
@@ -1,14 +1,53 @@
 package com.example.myapplication;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
 
 public class compare_player extends AppCompatActivity {
 
+    EditText next1 = (EditText) findViewById(R.id.search_player1);
+    ActivityResultLauncher<Intent> someActivityResultLauncher;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare_player);
+
+
+        next1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), PlayerList.class);
+
+                someActivityResultLauncher.launch(myIntent);
+            }
+
+
+        });
+        ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+        new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if (result.getResultCode() == Activity.RESULT_OK) {
+                            // There are no request codes
+                            Intent data = result.getData();
+                            doSomeOperations();
+                        }
+                    }
+                });
+
     }
+    private void doSomeOperations() {
+        next1.setText("hello");
+    }
+
 }
